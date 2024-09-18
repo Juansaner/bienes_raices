0683211db98246164715f0f155b4413acced2133
Juansaner <juan_san.erazo@uao.edu.co>
2024-09-18 09:03:43 -0500

    Hashear password

--- usuario.php
+++ usuario.php
@@ -8,10 +8,10 @@ $db = conectarDB();
 $email = "correo@correo.com";
 $password = "123456";
 
+$passwordHash = password_hash($password, PASSWORD_BCRYPT);
 //Query para crear el usuario
-$query = "INSERT INTO usuarios (email, password) VALUE ('$email', '$password'); ";
+$query = "INSERT INTO usuarios (email, password) VALUE ('$email', '$passwordHash'); ";
 
-echo $query;
 
 //Agregarlo a la base de datos
 mysqli_query($db, $query);
