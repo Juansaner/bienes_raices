894c2c51ee0787845c7d3e298af48e9af36ed35a
Juan Pablo <juansaner01@gmail.com>
2024-11-12 12:38:36 -0500

    Almacenando en la base de datos.
    Se modificó el código del query para insertar los valores en la base de datos.

--- classes/Propiedad.php
+++ classes/Propiedad.php
@@ -44,7 +44,11 @@ class Propiedad {
         $atributos = $this->sanitizarAtributos();
 
         //Insertar en la base de datos
-        $query = "INSERT INTO propiedades (titulo, precio, imagen, descripcion, habitaciones, wc, estacionamiento, creado, vendedores_id) VALUES ('$this->titulo', '$this->precio', '$this->imagen', '$this->descripcion','$this->habitaciones', '$this->wc', '$this->estacionamiento', '$this->creado', '$this->vendedores_id')";
+        $query = "INSERT INTO propiedades ( ";
+        $query .= join(', ', array_keys($atributos));
+        $query .= " ) VALUES (' "; 
+        $query .= join("', '", array_values($atributos));
+        $query .= " ') ";
         
         $resultado = self::$db->query($query);
         debuguear($resultado);
